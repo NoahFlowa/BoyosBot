54b28d8df3e7ecd9c917788db2a1e48c9b88f7ea
NoahFlowa <osterhoutn@gmail.com>
2023-04-19 17:38:45 -0400

    Potential fix for command problem and command.data.name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // * Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 // * Require ENV variables
 const { discordToken } = require('./config.json');
@@ -33,7 +33,12 @@ for (const file of eventFiles) {
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    client.commands.set(command.data.name, command);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
 }
 
 // * Log in to Discord with your client's token
