8ab8584c86b6af131706bc6f4520c3e42154dabc
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 15:50:51 -0400

    Updated user.js

--- commands/user.js
+++ commands/user.js
@@ -22,14 +22,16 @@ module.exports = {
 		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
 
+		const avatarURL = interaction.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
+
 		// Create the embed
 		const embed = new EmbedBuilder()
 			.setTitle('User Information')
 			.setColor(0x22c2fc)
 			.setDescription(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`)
 			.setTimestamp()
-			.setThumbnail({ url: interaction.user.avatarURL({ dynamic: true }) })
-			.setImage({ url: interaction.user.avatarURL({ dynamic: true }) })
+			.setThumbnail({ url: avatarURL })
+			.setImage({ url: avatarURL })
 			.setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
 
 		// Connect to the database
