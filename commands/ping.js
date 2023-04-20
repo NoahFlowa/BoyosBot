4858cad4b5e8054a137303bda3ac7b60633ca02c
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 15:38:19 -0400

    Updated ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,19 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		// Create the embed
+		const embed = new EmbedBuilder()
+			.setTitle('Pong!')
+			.setColor(0x000000)
+			.setDescription(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`)
+			.setTimestamp()
+            .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
+
+		// Reply with the embed
+		await interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
