c94fcb3e95204590b020c57cc31d7e1c465508ca
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 18:21:43 -0400

    Added function for connection to database

---
+++ functions/databaseConnection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+const { hostName, port, userName, password, databaseName } = require('./config.json');
+
+function connectToDatabase() {
+    var mysqlConnection = mysql.createConnection({
+        host: hostName,
+        port: port,
+        user: userName,
+        password: password,
+        database: databaseName
+    });
+
+    return mysqlConnection;
+}
+
+module.exports = {
+    connectToDatabase
+};
