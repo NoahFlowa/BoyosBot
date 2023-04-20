e5446b15e881d8c13a50177b5cfce8fdedbe8634
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 15:39:06 -0400

    Pong

--- commands/ping.js
+++ commands/ping.js
@@ -8,7 +8,7 @@ module.exports = {
 		// Create the embed
 		const embed = new EmbedBuilder()
 			.setTitle('Pong!')
-			.setColor(0x000000)
+			.setColor(0xffffff)
 			.setDescription(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`)
 			.setTimestamp()
             .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
