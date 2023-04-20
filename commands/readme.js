0c3ee26f91e9dfe73cda9d621e600a5255d54979
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 19:18:52 -0400

    Updated readme using similar method to changes

--- commands/readme.js
+++ commands/readme.js
@@ -1,5 +1,25 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
+const commandsArray = [
+    {
+        "commands": [
+            "/changes will tell you about the changes to the bot.",
+            "/clear will allow certain users to clear messages in a channel.",
+            "/cowboyindian will allow you to play the anual Cowboy vs. Indians game in the server.",
+            "/feature will allow you to request a feature to be added to the bot.",
+            "/gameserver will allow you to list, add, or remove a game server from the list.",
+            "/join will make the bot join the voice channel you are in.",
+            "/leave will make the bot leave the voice channel it is in.",
+            "/ping will allow you to see the ping of the bot.",
+            "/play will allow you to play music in the voice channel the bot is in.",
+            "/queue will allow you to see the queue of songs that are going to be played.",
+            "/readme will allow you to see the readme file for the bot.",
+            "/server will allow you to see information about the server.",
+            "/user will allow you to see information about you.",
+        ]
+    }
+];
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('readme')
@@ -12,30 +32,23 @@ module.exports = {
         .setAuthor({ name: '@NoahFlowa & @wymiller', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256', url: 'https://NoahOsterhout.com' })
         .setDescription('The Boyos Bot is a bot created by Noah Osterhout and Wyatt Miller. It is a bot that is used to play music in a voice channel, and to moderate the server. It is a work in progress, and is currently in beta. If you have any questions, please contact Noah Osterhout or Wyatt Miller.')
         .setThumbnail('https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=1024')
-        .addFields(
-            { name: 'Commands available for the bot', value: '\u200B' },
-            { name: 'ReadMe', value: 'Tells user about the bot', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Ping', value: 'Pong!', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Play', value: 'Pass a YouTube URL or search query for the bot to play music!', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Server', value: 'Tells user about the server', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Queue Play', value: 'Tells the bot to play the queue', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Join', value: 'Tells the bot to join the voice channel user is in', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Leave', value: 'Tells the bot to leave the voice channel', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Feature Request', value: 'Request a feature for the bot!  {5 Per Week}', inline: true },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Feature List', value: 'Tells the bot to grab all of your feature requests', inline: true },
-            { name: '\u200B', value: '\u200B' },
-        )
         .setImage('https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=1024')
         .setTimestamp()
         .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=1024'});
+
+        // loop through the commandsArray
+        let commandsText = '';
+
+        commandsArray.forEach((commandObj) => {
+            commandObj.command.forEach((command) => {
+                commandsText += `${command}\n`;
+            });
+        });
+
+        readmeEmbed.addFields(
+            { name: 'Commands available for the bot', value: '\u200B' },
+            { name: 'Command Name: ', value: changesText },
+        );
 
         await interaction.reply({ embeds: [readmeEmbed] });
 	},
