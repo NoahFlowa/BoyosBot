e704043dbc14fc18f0abc0f7a867367da6f84c7a
NoahFlowa <osterhoutn@gmail.com>
2023-04-20 12:54:04 -0400

    Formatting changes 2

--- commands/features.js
+++ commands/features.js
@@ -118,7 +118,6 @@ module.exports = {
                         .addFields(
                             { name: '\u200B', value: '\u200B'},
                             { name: 'Request', value: 'Date' },
-                            { name: '\u200B', value: '\u200B'},
                         )
                         .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
         
