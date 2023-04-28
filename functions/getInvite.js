ae8198765d294294c629a7d944993f6614d63bab
NoahFlowa <osterhoutn@gmail.com>
2023-04-28 14:05:22 -0400

    Working commit

--- functions/getInvite.js
+++ functions/getInvite.js
@@ -1,12 +1,14 @@
 async function getInvite(guild) {
-    const newInvites = await guild.invites.fetch(); // Update this line
+    const newInvites = await guild.invites.fetch();
     const oldInvites = guild.invites;
-    guild.invites = newInvites;
-    
+
     const invite = newInvites.find(inv => {
         const oldInvite = oldInvites.get(inv.code);
         return !oldInvite || inv.uses > oldInvite.uses;
     });
+
+    guild.invites = new Collection();
+    newInvites.each(invite => guild.invites.set(invite.code, invite));
 
     return invite;
 }
