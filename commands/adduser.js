c18a2898da07d89d820bfb52a20ca0e8201fa297
NoahFlowa <osterhoutn@gmail.com>
2023-05-05 18:05:14 -0400

    Revert to "Added error handling"
    This reverts commit 2edf313236997d414e790ad45d4a43c0985e8068.

--- commands/adduser.js
+++ commands/adduser.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
 // Import mysql connection
 const { connectToDatabase } = require('../functions/databaseConnection.js');
 const { getUserPermissions } = require('../functions/getUserPermissions.js');
-const { logError } = require('../functions/logError.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -23,61 +22,42 @@ module.exports = {
 
         // Check if user has permission to add users
         if (userPermissions == 1) {
-            try {
-                // Connect to database
-                var mysqlConnection = connectToDatabase();
-                mysqlConnection.connect();
+            // Connect to database
+            var mysqlConnection = connectToDatabase();
+            mysqlConnection.connect();
 
-                // Get values from interaction
-                const name = interaction.options.getString('name');
-                const discordUserName = interaction.options.getString('discordusername');
-                const discordUserID = interaction.options.getString('discorduserid');
-                const permissionID = interaction.options.getInteger('permissionid');
+            // Get values from interaction
+            const name = interaction.options.getString('name');
+            const discordUserName = interaction.options.getString('discordusername');
+            const discordUserID = interaction.options.getString('discorduserid');
+            const permissionID = interaction.options.getInteger('permissionid');
 
-                // Insert user into database
-                // Users table schema: (name, discordUserName, discordUderIS, permissionID)
-                mysqlConnection.query(`INSERT INTO Users (name, discordUserName, discordUserID, permissionID) VALUES ('${name}', '${discordUserName}', '${discordUserID}', '${permissionID}')`, function (err, result, fields) {
-                    if (err) {
-                        mysqlConnection.end();
-                        throw err;
-                    }
-
-                    // Create embed
-                    const embed = new EmbedBuilder()
-                    .setTitle("User added")
-                    .setDescription("User added to the database")
-                    .addFields({ name: 'Name', value: `${name}` })
-                    .addFields({ name: 'Discord username', value: `${discordUserName}` })
-                    .addFields({ name: 'Discord user ID', value: `${discordUserID}` })
-                    .addFields({ name: 'Permission ID', value: `${permissionID}` })
-                    .setColor(0x22c2fc)
-                    .setTimestamp()
-                    .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
-
-                    // Close connection
+            // Insert user into database
+            // Users table schema: (name, discordUserName, discordUderIS, permissionID)
+            mysqlConnection.query(`INSERT INTO Users (name, discordUserName, discordUserID, permissionID) VALUES ('${name}', '${discordUserName}', '${discordUserID}', '${permissionID}')`, function (err, result, fields) {
+                if (err) {
                     mysqlConnection.end();
-
-                    // Send embed
-                    interaction.reply({ embeds: [embed] });
-                });
-            } catch (err) {
-                // Get userID
-                const userID = interaction.user.id;
-
-                // Log error
-                logError(`${err}`, '../commands/adduser.js', `${userID}`);
+                    throw err;
+                }
 
                 // Create embed
                 const embed = new EmbedBuilder()
-                .setTitle("Error adding user")
-                .setDescription("An error occurred while adding the user to the database. The error has been logged and will be fixed as soon as possible.")
-                .setColor(0xff0000)
+                .setTitle("User added")
+                .setDescription("User added to the database")
+                .addFields({ name: 'Name', value: `${name}` })
+                .addFields({ name: 'Discord username', value: `${discordUserName}` })
+                .addFields({ name: 'Discord user ID', value: `${discordUserID}` })
+                .addFields({ name: 'Permission ID', value: `${permissionID}` })
+                .setColor(0x22c2fc)
                 .setTimestamp()
                 .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
 
+                // Close connection
+                mysqlConnection.end();
+
                 // Send embed
-                interaction.reply({ embeds: [embed], ephemeral: true });
-            }
+                interaction.reply({ embeds: [embed] });
+            });
         } else {
             // Create embed
             const embed = new EmbedBuilder()
@@ -88,7 +68,7 @@ module.exports = {
             .setFooter({ text: 'The Boyos Bot', iconURL: 'https://cdn.discordapp.com/avatars/1037147995940073533/cf9144e290ee7a0b8a06152ac8228410.png?size=256' });
 
             // Send embed
-            interaction.reply({ embeds: [embed], ephemeral: true });
+            interaction.reply({ embeds: [embed] });
         }
     },
 }
